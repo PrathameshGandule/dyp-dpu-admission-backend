4b699def9a20da62f8a9551a65adfc2f752258d5
PrathameshGandule <prathameshgandule1@gmail.com>
2025-02-25 15:07:30 +0530

    updated desk routes

--- routes/deskRoutes.js
+++ routes/deskRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyToken , verifyDesk } from "../middlewares/authMiddleware.js";
-import { releaseStudent, setStudent , updateDesk1 , updateDesk2, updateDesk3 } from "../controllers/deskCont.js";
+import { releaseStudent, setStudent , getDeskNullStudents , updateDesk1 , updateDesk2, updateDesk3 } from "../controllers/deskCont.js";
 
 const router = Router();
 
 router.post('/select/:id', verifyToken, verifyDesk("desk1", "desk2", "desk3"), setStudent);
 router.post('/release/:id', verifyToken, verifyDesk("desk1", "desk2", "desk3"), releaseStudent);
+router.get('/', verifyToken, verifyDesk("desk1", "desk2", "desk3"), getDeskNullStudents);
 router.put('/1/update/:id', verifyToken, verifyDesk("desk1"), updateDesk1);
 router.put('/2/update/:id', verifyToken, verifyDesk("desk2"), updateDesk2);
 router.put('/3/update/:id', verifyToken, verifyDesk("desk3"), updateDesk3);
