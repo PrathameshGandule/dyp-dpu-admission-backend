257a24922b011a49c0a255ad5fc249c2b7683a44
PrathameshGandule <prathameshgandule1@gmail.com>
2025-04-23 14:42:08 +0530

    Removed DYPDPU from token and made unique number's size from 5digit to 4digit

--- utils/generateId.js
+++ utils/generateId.js
@@ -1,11 +1,12 @@
 import Counter from "../models/Counter.js";
 
+// changed prefix to remove DYPDPU at start
 const getPrefix = (stream) => {
     const now = new Date();
     const month = now.toLocaleString("en-US", { month: "short" }).toUpperCase();
     const day = String(now.getDate()).padStart(2, "0");
     const year = now.getFullYear().toString().slice(2);
-    return `DYPDPU${stream.toUpperCase()}${day}${month}${year}`;
+    return `${stream.toUpperCase()}${day}${month}${year}`;
 }
 
 const getNextStudId = async (stream) => {
@@ -15,7 +16,8 @@ const getNextStudId = async (stream) => {
         { $inc: { sequence_value: 1 } },
         { new: true, upsert: true }
     );
-    let seq = String(counter.sequence_value).padStart(5, "0");
+    // made padstart to 4 for 4 digit unique numbers only
+    let seq = String(counter.sequence_value).padStart(4, "0");
     return currentPrefix + seq;
 };
 
