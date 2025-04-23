7d5deeeef315b9e05890ea899d199144d042f588
PrathameshGandule <prathameshgandule1@gmail.com>
2025-04-23 14:40:30 +0530

    Added a desk, removed subject marks, percentage to percentile for jee/cet in student model

--- models/Student.js
+++ models/Student.js
@@ -7,24 +7,22 @@ const studentSchema = new Schema({
     gender: { type: String, enum: ["male", "female", "other"], default: "male" },
     phone: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    purpose: { type: String, enum: ["admission", "inquiry", "visit"], required: true },
-    stream: { type: String, enum: ["eng", "mba", "phr"], required: true },
-    currentDesk: { type: String, enum: ["desk1", "desk2", "desk3", "completed"], default: "desk1" },
+    purpose: { type: String, enum: ["admission", "visit"], required: true }, // removed inquiry field
+    stream: { type: String, enum: ["eng", "mba", "phr", "libart"], required: true }, // added new field liberal arts
+    currentDesk: { type: String, enum: ["desk1", "desk2", "desk3", "desk4", "completed"], default: "desk1" }, // added new desk4
     visitors: { type: String, default: 0 },
     logs: [{
         entryTime: { type: Date, required: true },
-        exitTime: { type: Date, default: null }    
+        exitTime: { type: Date, default: null },
+        _id: false 
     }],
     desk_updates: {
         desk1: {
             counsellorId: { type: Schema.Types.ObjectId, ref: "Counsellor", default: null },
-            // studentName: { type: String, default: "" },
             fatherName: { type: String, default: "" },
             motherName: { type: String, default: "" },
             nationality: { type: String, default: "" },
             category: { type: String, default: "" },
-            // email: { type: String, default: "" },
-            // phone: { type: String, default: "", unique: true },
             address: { type: String, default: "" },
             pinCode: { type: String, default: "" },
             sscBoard: { type: String, default: "" },
@@ -34,30 +32,31 @@ const studentSchema = new Schema({
             sscPercentage: { type: String, default: "" },
             hscBoard: { type: String, default: "" },
             hscYear: { type: String, default: "" },
-            hscPhysics: { type: String, default: "" },
-            hscChemistry: { type: String, default: "" },
-            hscMaths: { type: String, default: "" },
-            hscTotalMarks: { type: String, default: "" },
+            hscMarks: { type: String, default: "" },
+            hscOutOf: { type: String, default: "" },
             hscPercentage: { type: String, default: "" },
             jeeYear: { type: String, default: "" },
-            jeePercentage: { type: String, default: "" },
+            jeePercentile: { type: String, default: "" },
             cetYear: { type: String, default: "" },
-            cetPercentage: { type: String, default: "" },
+            cetPercentile: { type: String, default: "" },
             enrollmentId: { type: String, default: "EN24" },
             branch: { type: String, default: "" },
-            remarks: { type: String, default: "No remarks currently" },
+            remarks: { type: String, default: "No remarks currently" }
+        },
+        desk2: {
             campusVisit: { type: Boolean, default: false },
             cafeteriaVisit: { type: Boolean, default: false },
             sportsFacilityVisit: { type: Boolean, default: false },
             labVisit: { type: Boolean, default: false },
             classroomVisit: { type: Boolean, default: false },
+            remarks: { type: String, default: "No remarks currently" }
         },
-        desk2: {
+        desk3: {
             counsellorId: { type: Schema.Types.ObjectId, ref: "Counsellor", default: null },
             topic: { type: String, default: "" },
             remarks: { type: String, default: "No remarks currently" }
         },
-        desk3: {
+        desk4: {
             counsellorId: { type: Schema.Types.ObjectId, ref: "Counsellor", default: null },
             admissionStatus: { type: Boolean, default: false },
             reason: { type: String, default: "" },
